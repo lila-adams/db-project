f6d1ddd3dfc6721ffd47e7ca7b6db45c44bdf21c
lila-adams <157045053+lila-adams@users.noreply.github.com>
2025-11-24 23:53:35 +0000

    added artist-db.php-- done with basic db queries!!

--- artist-db.php
+++ artist-db.php
@@ -1,11 +1,11 @@
 <?php
 
-# add author
-function addAuthor($name)
+# add artist
+function addartist($name)
 {
     global $db;
 
-    $query = "INSERT INTO Author (name) VALUES (:name)";  
+    $query = "INSERT INTO Artist (name) VALUES (:name)";  
     
     try {
         $statement = $db->prepare($query);    // compile, leave fill-in-the-blank
@@ -15,55 +15,54 @@ function addAuthor($name)
         $statement->closeCursor();
 
         if ($statement->rowCount() == 0)
-            echo "Failed to add author. <br/>";
+            echo "Failed to add artist. <br/>";
     }
     catch (PDOException $e) 
     {
-        echo "Failed to add author. Please ensure all fields are filled in.";
+        echo "Failed to add artist. Please ensure all fields are filled in.";
     }
     catch (Exception $e)
     {
-       echo "Failed to add author. Please ensure all fields are filled in.";
+       echo "Failed to add artist. Please ensure all fields are filled in.";
     }
 }
 
 
-function addAuthorToEntry($entry_id, $author_id, $year)
+function addartistToEntry($entry_id, $artist_id)
 {
     global $db;
 
-    $query = "INSERT INTO written_by (entry_id, author_id, year) VALUES (:entry_id, :author_id, :year)";  
+    $query = "INSERT INTO drawn_by (entry_id, artist_id) VALUES (:entry_id, :artist_id)";  
     
     try {
         $statement = $db->prepare($query);    // compile, leave fill-in-the-blank
         $statement->bindValue(':entry_id', $entry_id);
-        $statement->bindValue(':author_id', $author_id);
-        $statement->bindValue(':year', $year);
+        $statement->bindValue(':artist_id', $artist_id);
         $statement->execute();      // run 
 
         $statement->closeCursor();
 
         if ($statement->rowCount() == 0)
-            echo "Failed to add author. <br/>";
+            echo "Failed to add artist. <br/>";
     }
     catch (PDOException $e) 
     {
-        echo "Failed to add author. Please ensure all fields are filled in.";
+        echo "Failed to add artist. Please ensure all fields are filled in.";
     }
     catch (Exception $e)
     {
-       echo "Failed to add author. Please ensure all fields are filled in.";
+       echo "Failed to add artist. Please ensure all fields are filled in.";
     }
 }
 
-# get author info from entry_id
-function getAuthorByEntry($entry_id, $user_id)
+# get artist info from entry_id
+function getartistByEntry($entry_id, $user_id)
 {
     global $db;
-    $query = "SELECT * FROM Author 
-    JOIN written_by ON Author.author_id = written_by.author_id
-    LEFT JOIN Entry on written_by.entry_id = Entry.entry_id
-    WHERE written_by.entry_id = :entry_id
+    $query = "SELECT * FROM Artist 
+    JOIN drawn_by ON Artist.artist_id = drawn_by.artist_id
+    LEFT JOIN Entry on drawn_by.entry_id = Entry.entry_id
+    WHERE drawn_by.entry_id = :entry_id
     AND Entry.user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(':entry_id', $entry_id);
@@ -77,17 +76,17 @@ function getAuthorByEntry($entry_id, $user_id)
 
 
 
-# get ALL entries by Author
-function getEntriesByAuthor($author_id, $user_id)
+# get ALL entries by artist
+function getEntriesByArtist($artist_id, $user_id)
 {
     global $db;
     $query = "SELECT * FROM Entry 
-    JOIN written_by ON Author.author_id = written_by.author_id
-    LEFT JOIN Entry on written_by.entry_id = Entry.entry_id
-    WHERE Author.author_id = :author_id
+    JOIN drawn_by ON Artist.artist_id = drawn_by.artist_id
+    LEFT JOIN Entry on drawn_by.entry_id = Entry.entry_id
+    WHERE Artist.artist_id = :artist_id
     AND Entry.user_id = :user_id";
     $statement = $db->prepare($query);
-    $statement->bindValue(':author_id', $author_id);
+    $statement->bindValue(':artist_id', $artist_id);
     $statement->bindValue(':user_id', $user_id);
     $statement->execute();
     $results = $statement->fetch();   // fetch()
@@ -97,52 +96,52 @@ function getEntriesByAuthor($author_id, $user_id)
 }
 
 
-function delete_author($author_id) {
+function delete_artist($artist_id) {
     global $db;
 
-    $query = "DELETE FROM Author WHERE author_id=:author_id";
+    $query = "DELETE FROM Artist WHERE artist_id=:artist_id";
 
     $statement = $db->prepare($query);
-    $statement->bindValue(':author_id', $author_id);
+    $statement->bindValue(':artist_id', $artist_id);
     $statement->execute();    
     
     $statement->closeCursor();
 }
 
-function deleteWrittenBy($author_id, $entry_id) {
+function deleteDrawnBy($artist_id, $entry_id) {
     global $db;
 
-    $query = "DELETE FROM written_by WHERE author_id=:author_id AND entry_id =:entry_id";
+    $query = "DELETE FROM drawn_by WHERE artist_id=:artist_id AND entry_id =:entry_id";
 
     $statement = $db->prepare($query);
-    $statement->bindValue(':author_id', $author_id);
+    $statement->bindValue(':artist_id', $artist_id);
     $statement->bindValue(':entry_id', $entry_id);
     $statement->execute();    
     
     $statement->closeCursor();
 }
 
-function updateAuthor($author_id, $name)
+function updateArtist($artist_id, $name)
 {
     global $db;
     
-    $query = "UPDATE Author SET name=:name WHERE author_id=:author_id";
+    $query = "UPDATE Artist SET name=:name WHERE artist_id=:artist_id";
     
     $statement = $db->prepare($query);    // compile, leave fill-in-the-blank
-    $statement->bindValue(':author_id', $author_id);
+    $statement->bindValue(':artist_id', $artist_id);
     $statement->bindValue(':name', $name);
     $statement->execute();      // run
 
     $statement->closeCursor();
 
 }
-function updateWrittenBy($entry_id, $author_id) {
+function updateDrawnBy($entry_id, $artist_id) {
     global $db;
     
-    $query = "UPDATE written_by SET author_id=:author_id WHERE entry_id=:entry_id";
+    $query = "UPDATE drawn_by SET artist_id=:artist_id WHERE entry_id=:entry_id";
     
     $statement = $db->prepare($query);    // compile, leave fill-in-the-blank
-    $statement->bindValue(':author_id', $author_id);
+    $statement->bindValue(':artist_id', $artist_id);
     $statement->bindValue(':entry_id', $entry_id);
     $statement->execute();      // run
 
