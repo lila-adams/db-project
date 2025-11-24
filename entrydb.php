1f749f5c73da35941f74de4e928d1a7f228fa8a6
lila-adams <157045053+lila-adams@users.noreply.github.com>
2025-11-24 22:16:59 +0000

    completed entrydb db all queries

--- entrydb.php
+++ entrydb.php
@@ -35,7 +35,7 @@ function getAllEntries($user_id)
 {
     global $db;
 
-    $query = "SELECT * FROM Entries WHERE user_id = :user_id";
+    $query = "SELECT * FROM Entry WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(':user_id', $user_id);
     $statement->execute();
@@ -44,6 +44,86 @@ function getAllEntries($user_id)
    
     return $results;
 }
+
+function searchEntries($user_id, $comic_name, $rating, $curr_status, $tag_text, $author_name, $artist_name)
+{
+    global $db;
+
+    $query = "SELECT * FROM Entry e
+        LEFT JOIN written_by wb on e.entry_id = wb.entry_id
+        LEFT JOIN drawn_by db on e.entry_id = db.entry_id
+        LEFT JOIN tag on tag.entry_id = e.entry_id
+        LEFT JOIN tag_map on tag.tag_id = tag_map.tag_id 
+        LEFT JOIN Artist on db.artist_id = Artist.artist_id
+        LEFT JOIN Author on wb.author_id = Author.author_id
+
+        WHERE user_id = :user_id";
+
+    if ($comic_name !== "") {
+        $query .= " AND comic_name LIKE :comic_name";
+    }
+    if ($tag_text !== "") {
+        $query .= " AND tag_text = :tag_text";
+    }
+    if ($rating !== "") {
+        $query .= " AND rating = :rating";
+    }
+    if ($curr_status !== "") {
+        $query .= " AND curr_status = :curr_status";
+    }
+    if ($author_name !== "") {
+        $query .= " AND author_name LIKE :author_name";
+    }
+    if ($artist_name !== "") {
+        $query .= " AND artist_name LIKE :artist_name";
+    }
+    
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':user_id', $user_id);
+    
+    // optional binds
+    if ($comic_name !== "") {
+        $statement->bindValue(':comic_name', "%$comic_name%");
+    }
+    if ($rating !== "") {
+        $statement->bindValue(':rating', $rating);
+    }
+    if ($curr_status !== "") {
+        $statement->bindValue(':curr_status', $curr_status);
+    }
+    if ($tag_id !== "") {
+        $statement->bindValue(':tag_text', $tag_text);
+    }
+    if ($author_id !== "") {
+        $statement->bindValue(':author_name', $author_name);
+    }
+    if ($artist_id !== "") {
+        $statement->bindValue(':artist_name', $artist_name);
+    }
+
+    $statement->execute();
+    $results = $statement->fetchAll();   // fetch()
+    $statement->closeCursor();
+   
+    return $results;
+}
+
+
+function findFavoriteEntries($user_id) {
+    global $db;
+
+    $stmt = $db->prepare("CALL FindFavoriteComics(:user_id)");
+    $stmt->bindValue(":user_id", $user_id);
+
+    $stmt->execute();
+    $results = $stmt->fetchAll();
+    $stmt->closeCursor();
+
+    return $results;
+}
+
 
 //get specific entry
 function getEntryById($entry_id)  
@@ -60,6 +140,7 @@ function getEntryById($entry_id)
     return $results;
 
 }
+
 
 //update entry info
 function updateEntry($entry_id, $comic_name, $rating, $user_id, $curr_status, $review)
