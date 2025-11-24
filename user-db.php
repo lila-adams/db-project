38b1ab1cc38822c50ec163105d3d7fc96adfb93c
lila-adams <157045053+lila-adams@users.noreply.github.com>
2025-11-24 23:32:43 +0000

    added user-db.php

---
+++ user-db.php
@@ -0,0 +1,105 @@
+<?php
+# add user
+
+function addUser($username, $pwd)
+{
+    global $db;
+
+    $query = "INSERT INTO User (username, pwd) VALUES (:username, :pwd)";  
+    
+    try {
+        $statement = $db->prepare($query);    // compile, leave fill-in-the-blank
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':pwd', $pwd);
+        $statement->execute();      // run 
+
+        $statement->closeCursor();
+
+        if ($statement->rowCount() == 0)
+            echo "Failed to add user. <br/>";
+    }
+    catch (PDOException $e) 
+    {
+        echo "Failed to add user. Please ensure all fields are filled in.";
+    }
+    catch (Exception $e)
+    {
+       echo "Failed to add user. Please ensure all fields are filled in.";
+    }
+}
+
+// get all users
+function getAllUsers()
+{
+    global $db;
+
+    $query = "SELECT * FROM User";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll();   // fetch()
+    $statement->closeCursor();
+   
+    return $results;
+}
+
+function getUserInfo($user_id)
+{
+    global $db;
+
+    $query = "SELECT * FROM User WHERE user_id =:user_id;";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':user_id', $goal_id);
+    $statement->execute();
+    $results = $statement->fetch();   // fetch()
+    $statement->closeCursor();
+   
+    return $results;
+}
+
+# could use this to verify that the user put in the correct password
+function getPwd_byUsername($username)
+{
+    global $db;
+
+    $query = "SELECT pwd FROM User WHERE username =:username;";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':username', $username);
+    $statement->execute();
+    $results = $statement->fetch();   // fetch()
+    $statement->closeCursor();
+   
+    return $results;
+}
+
+
+function delete_user($user_id) {
+    global $db;
+
+    $query = "DELETE FROM User WHERE user_id=:user_id";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':user_id', $user_id);
+    $statement->execute();    
+    
+    $statement->closeCursor();
+}
+
+# if they want to change their password? idk
+function updateUser($user_id, $username, $pwd)
+{
+    global $db;
+    
+    $query = "UPDATE User SET username=:username, pwd=:pwd WHERE user_id=:user_id";
+    
+    $statement = $db->prepare($query);    // compile, leave fill-in-the-blank
+    $statement->bindValue(':user_id', $user_id);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':pwd', $pwd);
+    $statement->execute();      // run
+
+    $statement->closeCursor();
+
+}
+
+
+?>
